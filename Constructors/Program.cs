6322d78e61f80e978f9ee448d4d20cdbc37dc3bd
Doğan DELLAL <dogandellal@gmail.com>
2021-09-11 23:54:31 +0300

    11.09.2021 Ders Sonu

---
+++ Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+        }
+    }
+
+    class Customer
+    {
+        public Customer()
+        {
+            Console.WriteLine("Constructor çalıştı.");
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ ReferansTypeSample/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ReferansTypeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            Customer customer1 = new Customer();
+            customer1.FirstName = "Doğan";
+
+            Employee employee = new Employee() { FirstName = "Uğur" };
+
+            Person person1 = new Person();
+            person1.FirstName ="Yusuf";
+
+            Person person2 = customer1;
+            
+
+            PersonManager personManager = new PersonManager();
+            personManager.Add(customer1);
+
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Phone { get; set; }
+        }
+        public class Employee : Person
+        {
+            public string Job { get; set; }
+        }
+        public class Customer : Person
+        {
+            public bool IsActive { get; set; }
+        }
+
+        public class PersonManager
+        {
+            public void Add(Person person)
+            {
+                Console.WriteLine("Eklendi" + person.FirstName);
+            }
+        }
+    }
+}
+
+
